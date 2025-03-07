a9174d0454278eeed464b7044c5e77c0bd85ec87
emrekkonakk <148265327+emrekkonakk@users.noreply.github.com>
2025-03-07 10:42:24 +0300

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -24,7 +24,8 @@ def create_app():
 
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", 'default_secret_key')
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL",
-                                                      "mysql+pymysql://root:Bibendum1.@localhost/internship")
+                                                      "mysql+pymysql://root:password.@localhost/internship") 
+    #changed password for safety
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
